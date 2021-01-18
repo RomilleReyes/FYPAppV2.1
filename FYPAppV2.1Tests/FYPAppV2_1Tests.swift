9b82b4113e56b71ef5fe8b964f1661226ca632ba
RomilleReyes <1719415@brunel.ac.uk>
2021-01-18 23:46:16 +0000

    Initial Commit

---
+++ FYPAppV2.1Tests/FYPAppV2_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  FYPAppV2_1Tests.swift
+//  FYPAppV2.1Tests
+//
+//  Created by Romille Reyes on 18/01/2021.
+//
+
+import XCTest
+@testable import FYPAppV2_1
+
+class FYPAppV2_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
